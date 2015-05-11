b8e4062f749211a992b1ff574c314db4ed83aa39
tmathew87 <tmathew87@gmail.com>
2015-05-11 09:57:04 -0500

    Data object

---
+++ app/src/main/java/com/uta/shoeperstar/vibe/Utilities/Data.java
@@ -0,0 +1,48 @@
+package com.uta.shoeperstar.vibe.Utilities;
+
+/**
+ * Created by saads_000 on 4/6/2015.
+ */
+public class Data
+{
+
+    public int startT;
+    public int startD;
+    public int data;
+
+    public Data() {
+    }
+
+    public Data(int Data, int start, int startDate)
+    {
+        this.startT=start;
+        this.startD=startDate;
+        this.data=Data;
+    }
+
+    public int getTime() {
+        return startT;
+    }
+
+    public void setTime(int startTime) {
+        this.startT = startTime;
+    }
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+
+    public int getDate(){
+        return startD;
+    }
+
+    public void setDate(int setDate) {
+        this.startD = setDate;
+    }
+
+
+}
